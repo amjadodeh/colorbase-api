047652b8c237d97dee57e8e0685b2b91c5c0756b
amjad <aao001056485@gmail.com>
2021-02-08 22:01:25 -0500

    removed "/" :p

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL,
-  CLIENT_ORIGIN: 'https://colorstop-client.vercel.app/',
+  CLIENT_ORIGIN: 'https://colorstop-client.vercel.app',
 };
